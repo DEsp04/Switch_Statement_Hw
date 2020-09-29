2164d296ff8138b782f3acc9fbda10e80b621201
david espinal <davidespinal@davids-MacBook-Pro.local>
2020-09-29 15:15:44 -0400

    Almost completed the switch statement

--- switch.js
+++ switch.js
@@ -1,3 +1,26 @@
-switch () { 
-  
+let price;
+let restItem = "Steak";
+
+switch (restItem) {
+  case "Burger":
+    price = "$10";
+    console.log(`${restItem}; ${price}`);
+    break;
+  case "Steak":
+    price = "$25";
+    console.log(`${restItem}; ${price}`);
+    break;
+  case "Salmon":
+    price = "$20";
+    console.log(`${restItem}; ${price}`);
+    break;
+  case "Shrimp":
+    price = "$20";
+    console.log(`${restItem}; ${price}`);
+    break;
+  case "Chicken":
+    price = "$15";
+    console.log(`${restItem}; ${price}`);
+    break;
+  default
 }
